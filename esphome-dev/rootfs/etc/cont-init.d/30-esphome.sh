5d23b1be7fa1b182d63968e20fcf130f7662ae8a
thinky123 <70031793+thinky123@users.noreply.github.com>
2020-08-21 23:18:42 +0530

    Add files via upload

---
+++ esphome-dev/rootfs/etc/cont-init.d/30-esphome.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Community Hass.io Add-ons: ESPHome
+# This files installs the user ESPHome version if specified
+# ==============================================================================
+
+declare esphome_version
+
+if bashio::config.has_value 'esphome_version'; then
+    esphome_version=$(bashio::config 'esphome_version')
+    full_url="https://github.com/esphome/esphome/archive/${esphome_version}.zip"
+    bashio::log.info "Installing esphome version '${esphome_version}' (${full_url})..."
+    pip2 install -U --no-cache-dir "${full_url}" \
+      || bashio::exit.nok "Failed installing esphome pinned version."
+fi
